33ef7ca4c5a7655ea6c0d0995135bc53863bf3d8
Brett Gilio <brettg@posteo.net>
2019-05-29 11:56:00 -0500

    Update sibble.c: Define keepalive

--- sibble.c
+++ sibble.c
@@ -21,6 +21,20 @@ struct randr {
   int err_root;
 };
 
+// Static definitions
+
+static void
+keepalive(void) {
+  FILE *f;
+  const char oomfile[]= "/proc/self/oom_score_adj";
+
+  if (!(f = fopen(oomfile, "w"))) {
+    if (errno == ENOENT)
+      return;
+    // add killproc
+  }
+}
+
 int
 main(int argc, char **argv) {
 
