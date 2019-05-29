d276b6e4a0fdeec8e84bcbc6282df71e87103791
Brett Gilio <brettg@posteo.net>
2019-05-28 20:32:37 -0500

    Add sibble.c

---
+++ sibble.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#if HAVE_SHADOW_H
+#include <shadow.h>
+#endif
+
+struct locker {
+  
+};
+
+struct randr {
+  int is_active;
+  int env_root;
+  int err_root;
+};
+
+int
+main(int argc, char **argv) {
+
+  return 0;
+}
